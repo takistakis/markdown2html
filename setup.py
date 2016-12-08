1f764dae14aa087050da49a7afc6873207274619
Panagiotis Ktistakis <panktist@gmail.com>
2016-12-08 23:47:00 +0200

    Add setup.py and .gitignore

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""setuptools installer script for markdown2html."""
+
+from setuptools import setup
+
+setup(
+    name='markdown2html',
+    version='0.1.0',
+    description='Convert a GitHub Flavored Markdown file to HTML.',
+    author='Panagiotis Ktistakis',
+    author_email='panktist@gmail.com',
+    url='https://github.com/forkbong/markdown2html',
+    license='GPLv3',
+    py_modules=['markdown2html'],
+    entry_points={'console_scripts': ['markdown2html=markdown2html:main']},
+    install_requires=['docopt', 'Markdown', 'Pygments'],
+)
